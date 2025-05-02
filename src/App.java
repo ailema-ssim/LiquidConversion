1f1ca4633338ee8dcec7e07d09acb713f016335d
ailema-ssim <205326731+ailema-ssim@users.noreply.github.com>
2025-05-02 10:58:00 -0400

    fixed more typos and swapped from while to if

--- src/App.java
+++ src/App.java
@@ -7,17 +7,26 @@ public class App {
         String measurementType = input.nextLine();
         Double d = 4.22675284;
         float conversionNumber = d.floatValue();
-        while (measurementType == "cups") {
+        if (measurementType == "cups")
+        {
            System.out.println("How many cups?"); 
            Float cupsAmount = input.nextFloat();
            Float cupsToLitersAmount = cupsAmount / conversionNumber;
            System.out.println(cupsAmount + "Cups is" + cupsToLitersAmount + " Liters");
-        while (measurementType == "liters") {
+        }
+        if (measurementType == "liters") 
+        {
         System.out.println("How many liters?"); 
            Float litersAmount = input.nextFloat();
            Float litersToCupsAmount = litersAmount / conversionNumber;
-           System.out.println(cupsAmount + "Liters is" + litersToCupsAmount + " Cups");
+           System.out.println(litersAmount + "Liters is" + litersToCupsAmount + " Cups");
         }
+        else
+        {
+            System.out.println("Measurement type must be cups or liters.");
+
+        }
+        
 
 
         
