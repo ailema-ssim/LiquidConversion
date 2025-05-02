2b6582e8e0a38fb9eebce5ca02a7faf28f4830dd
ailema-ssim <205326731+ailema-ssim@users.noreply.github.com>
2025-05-02 10:37:26 -0400

    added liters conversion

--- src/App.java
+++ src/App.java
@@ -7,11 +7,17 @@ public class App {
         String measurementType = input.nextLine();
         Double d = 4.22675284;
         float conversionNumber = d.floatValue();
-        if (measurementType == "cups") {
+        while (measurementType == "cups") {
            System.out.println("How many cups?"); 
            Float cupsAmount = input.nextFloat();
            Float cupsToLitersAmount = cupsAmount / conversionNumber;
            System.out.println(cupsAmount + "Cups is" + cupsToLitersAmount + " Liters");
+        while (measurementType == "liters") {
+        System.out.println("How many liters?"); 
+           Float litersAmount = input.nextFloat();
+           Float litersToCupsAmount = litersAmount / conversionNumber;
+           System.out.println(cupsAmount + "Liters is" + litersToCupsAmount + " Cups");
+        }
 
 
         
