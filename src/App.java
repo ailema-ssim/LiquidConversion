fdeefe36e46887e526de493d2f659f82b48d583b
ailema-ssim <205326731+ailema-ssim@users.noreply.github.com>
2025-05-06 09:19:37 -0400

    Moved class conversion

--- src/App.java
+++ src/App.java
@@ -1,29 +1,33 @@
 import java.util.InputMismatchException;
 import java.util.Scanner;
 
+// public class conversion;
+// public static void main(String[] args) throws Exception {
+//     Scanner input = new Scanner(System.in);
+//     System.out.println("Is the measurement in cups or liters?");
+//     String measurementType = input.nextLine();
+//     Double d = 4.22675284;
+//     float conversionNumber = d.floatValue();
+//     if (measurementType.equalsIgnoreCase("cups")) {
+//         System.out.println("How many cups?");
+//         Float cupsAmount = input.nextFloat();
+//         Float cupsToLitersAmount = cupsAmount / conversionNumber;
+//         System.out.println(cupsAmount + " Cups is " + cupsToLitersAmount + " Liters");
+//     } else if (measurementType.equalsIgnoreCase("liters")) {
+//         System.out.println("How many liters?");
+//         Float litersAmount = input.nextFloat();
+//         Float litersToCupsAmount = litersAmount / conversionNumber;
+//         System.out.println(litersAmount + " Liters is " + litersToCupsAmount + " Cups");
+//     } else {
+//         System.out.println("Measurement type must be cups or liters.");
+//         System.out.println("Is the measurement in cups or liters?");
+//         measurementType = input.nextLine();
+//     }
+// }
+
 public class App {
-    public static void main(String[] args) throws Exception {
-        Scanner input = new Scanner(System.in);
-        System.out.println("Is the measurement in cups or liters?");
-        String measurementType = input.nextLine();
-        Double d = 4.22675284;
-        float conversionNumber = d.floatValue();
-        if (measurementType.equalsIgnoreCase("cups")) {
-            System.out.println("How many cups?");
-            Float cupsAmount = input.nextFloat();
-            Float cupsToLitersAmount = cupsAmount / conversionNumber;
-            System.out.println(cupsAmount + " Cups is " + cupsToLitersAmount + " Liters");
-        } else if (measurementType.equalsIgnoreCase("liters")) {
-            System.out.println("How many liters?");
-            Float litersAmount = input.nextFloat();
-            Float litersToCupsAmount = litersAmount / conversionNumber;
-            System.out.println(litersAmount + " Liters is " + litersToCupsAmount + " Cups");
-        } else {
-            System.out.println("Measurement type must be cups or liters.");
-            System.out.println("Is the measurement in cups or liters?");
-            measurementType = input.nextLine();
-        }
-        boolean a = false;
+    
+        boolean a = false;{
         do {
             Scanner b = new Scanner(System.in);
             try {
@@ -32,8 +36,8 @@ public class App {
                 Boolean yesOrNo = b.nextBoolean();
 
                 if (yesOrNo == true) {
-                    System.out.println("Is the measurement in cups or liters?");
-                    measurementType = input.nextLine();
+                    continue conversion;
+                    
                 } else if (yesOrNo == false) {
                     System.out.println("Have a good day");
                 }
@@ -44,6 +48,7 @@ public class App {
             }
         } while (!a);
 
-    }
-
 }
+}
+
+
